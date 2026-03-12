df38bdb3cd2a64a4f39566465ffa2325c23e6aa2
Rishit <gokalnirishit001@gmail.com>
2026-03-13 00:53:48 +0530

    keymapping for kotlin

--- lua/rishit_configs/remap.lua
+++ lua/rishit_configs/remap.lua
@@ -2,14 +2,23 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>f", vim.cmd.Ex)
 
 vim.keymap.set("i", "<S-CR>", "<Esc>o", { noremap= true, silent = true})
+local function run_file()
+    vim.cmd('w')
+    local ext = vim.fn.expand('%:e')
 
-vim.api.nvim_exec([[
-    nnoremap <leader>r :w<CR>:!javac % && java %:r<CR>
-    ]], false)
 
-
+    if ext == 'java' then
+	vim.cmd('!javac % && java %:r')
+    elseif ext == 'kt' then
+	vim.cmd('!kotlinc % -include-runtime -d %:r.jar && java -jar %:r.jar')
+    elseif ext == 'py' then
+	vim.cmd('!python %')
+    end
+end
+vim.keymap.set("n", "<leader>r", run_file)
 vim.api.nvim_set_keymap('i', '{', '{}<Esc>i', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '(', '()<Esc>i', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '"', '""<Esc>i', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', "'", "''<Esc>i", { noremap = true, silent = true })
+
 
